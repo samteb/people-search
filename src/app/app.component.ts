e6e7c66ce21bc8c81e7a4f2f314837420204ac9b
samteb <sam.teboul@gmail.com>
2019-10-22 21:48:25 +0300

    move debounce operator to searchText

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { combineLatest, Observable } from 'rxjs';
-import { map, startWith, debounceTime } from 'rxjs/operators';
+import { map, startWith, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { User } from './models/user.model';
 import { ApiService } from './services/api.service';
 
@@ -24,10 +24,13 @@ export class AppComponent {
     private apiService: ApiService,
   ) {
     this.searchText = new FormControl('');
-    this.searchText$ = this.searchText.valueChanges.pipe(startWith(''));
+    this.searchText$ = this.searchText.valueChanges.pipe(
+      startWith(''),
+      debounceTime(400),
+      distinctUntilChanged()
+    );
     this.users$ = this.apiService.getUsers(100);
     this.filteredUsers$ = combineLatest(this.users$, this.searchText$).pipe(
-      debounceTime(500),
       map(([users, searchText]) => users.filter(user => user.username.includes(searchText.toLowerCase()) || user.email.includes(searchText.toLowerCase())))
     );
     this.searchText$.subscribe(value => this.disableBtn = value.length === 0);
